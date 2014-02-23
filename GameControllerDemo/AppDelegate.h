2c904d0fc24d795fb0b384b42c7786a2f6a335d3
Andrew Bowers <grillaface@gmail.com>
2014-02-23 14:59:24 -0500

    Initial Commit

---
+++ GameControllerDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GameControllerDemo
+//
+//  Created by Andrew Bowers on 2/23/14.
+//  Copyright (c) 2014 GrillaDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GameControllerDemo/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  GameControllerDemo
+//
+
+//  Copyright (c) 2014 GrillaDev. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
